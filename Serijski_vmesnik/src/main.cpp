dba0570881e1be0a0571e55b07e2d03b0b5aa8cc
leonbl <leon.blazic@gmail.com>
2023-09-12 14:02:24 +0200

    Update main.cpp

--- Serijski_vmesnik/src/main.cpp
+++ Serijski_vmesnik/src/main.cpp
@@ -1,11 +1,18 @@
 #include <Arduino.h>
-
-
+int stevec=0;
 void setup() {
-
+  Serial.begin(9600);
 }
 
 void loop() {
-
+  if(Serial.available()!=0){
+    int stevilo = Serial.available();
+    Serial.print("Imam ");
+    Serial.print(stevilo);
+    Serial.print(" znakov, prebral sem: ");
+    char crka = Serial.read();
+    Serial.println(crka);
+  }
+  
 }
 
