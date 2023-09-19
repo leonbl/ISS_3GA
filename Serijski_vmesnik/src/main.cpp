1a79ebaf2797c91ad1b0312941c7fad5aae41b33
leonbl <leon.blazic@gmail.com>
2023-09-19 14:02:28 +0200

    Update main.cpp

--- Serijski_vmesnik/src/main.cpp
+++ Serijski_vmesnik/src/main.cpp
@@ -1,18 +1,28 @@
 #include <Arduino.h>
-int stevec=0;
-void setup() {
+int stevec = 0;
+void setup()
+{
   Serial.begin(9600);
+  pinMode(10, OUTPUT);
+  pinMode(11, OUTPUT);
+  pinMode(12, OUTPUT);
+  pinMode(13, OUTPUT);
+  digitalWrite(10, HIGH);
+  digitalWrite(11, HIGH);
+  digitalWrite(12, HIGH);
+  digitalWrite(13, HIGH);
 }
 
-void loop() {
+void loop()
+{
   if(Serial.available()!=0){
-    int stevilo = Serial.available();
-    Serial.print("Imam ");
-    Serial.print(stevilo);
-    Serial.print(" znakov, prebral sem: ");
     char crka = Serial.read();
     Serial.println(crka);
+    if(isDigit(crka) == 1){
+      if(crka=='1'){
+        digitalWrite(10, LOW);
+      }
+    }
   }
-  
-}
 
+}
